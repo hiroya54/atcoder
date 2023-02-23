d527d3f07eeb8e9cc76d10743d04aeb43a8c0726
hiroya54 <hiroya.0504@icloud.com>
2023-02-23 12:46:27 +0900

    ファイル修正

--- ShortestPath/Main_dijkstra1.java
+++ ShortestPath/Main_dijkstra1.java
@@ -1,100 +1,80 @@
-package shortest_path.code14_3;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
-//重みと頂点
+//重み付きグラフの辺
 class Edge{
-	private int to; //辺を結ぶ頂点
-	private int w; //重み
-	Edge(int to,int w){ //辺を結ぶ頂点と重みが格納
-		this.to=to;
-		this.w=w;
-	}
-	public int getTo() {
-		return to;
-	}
-	public void setTo(int to) {
+	int to; //隣接頂点番号
+	int w; //重み
+	//コンストラクタ
+	Edge(int to,int w){
 		this.to = to;
-	}
-	public int getW() {
-		return w;
-	}
-	public void setW(int w) {
 		this.w = w;
-	}	
+	}
 }
 
 public class Main {
-	final static int INF = 999999999;
-	static int[] dist;
-	static boolean[] used;
-	
-	//重み付きグラフ
-	public static ArrayList<ArrayList<Edge>> graph_edge(int n) { //頂点nの重み付きグラフ作成
-		ArrayList<ArrayList<Edge>> g = new ArrayList<>(); //重み付きなので、結ぶ頂点と重みの情報
+	static int INF = 999999999;
+	//重み付きグラフ作成メソッド
+	public static ArrayList<ArrayList<Edge>> makeWGraph(int n){
+		ArrayList<ArrayList<Edge>> g = new ArrayList<>();
 		for(int i=0;i<n;i++) {
-			ArrayList<Edge> v = new ArrayList<>();
-			g.add(v);
+			g.add(new ArrayList<Edge>());
 		}
 		return g;
+	
 	}
-	//緩和を実施する関数
-	public static boolean chmin(int a,int b) {
-		if(a>b) {
-			return true;
-		}else {
-			return false;
-		}
-	}
-	public static void main(String[] args) {
-		//インプット
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(); //頂点の数
-		int m = sc.nextInt(); //辺の数
-		//int s = sc.nextInt(); //始点
+	public static void main(String[] args) throws IOException{
+		//入力
+		BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(buff.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 		
-		//グラフ作成
-		ArrayList<ArrayList<Edge>> g = graph_edge(n);
+		//頂点nの重み付きグラフ作成
+		ArrayList<ArrayList<Edge>> g = makeWGraph(n);
 		for(int i=0;i<m;i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			int w = sc.nextInt();
-			Edge e = new Edge(b,w);
-			g.get(a).add(e);
+			st = new StringTokenizer(buff.readLine());
+			int a = Integer.parseInt(st.nextToken())-1;
+			int b = Integer.parseInt(st.nextToken())-1;
+			int c = Integer.parseInt(st.nextToken());
+			g.get(a).add(new Edge(b,c));
+			g.get(b).add(new Edge(a,c));
 		}
 		
 		//ダイクストラ法
-		used = new boolean[n];
-		Arrays.fill(used, false);
-		dist = new int[n];
-		Arrays.fill(dist,INF);
+		boolean[] seen = new boolean[n];
+		int[] dist = new int[n];
+		Arrays.fill(dist, INF);
 		dist[0]=0;
-		//O(V^2)
-		for(int i=0;i<n;i++) { //O(V)
-			//使用済みでない頂点のうち、dist値が最小の頂点を探す
-			int min_dist=INF;
-			int min_v =-1;
-			for(int v=0;v<n;v++) { // O(V)
-				if(!used[v] && dist[v]<min_dist) {
+		
+		for(int i=0;i<n;i++) {
+			//seen=falseのうち、distが最小の頂点を探す
+			int min_dist = INF;
+			int min_v = -1;
+			for(int v=0;v<n;v++) {
+				if(!seen[v] && dist[v]<min_dist) {
 					min_dist=dist[v];
 					min_v=v;
 				}
 			}
-			//もしそのような頂点が見つからなければ終了する
+			//終了条件
 			if(min_v==-1) break;
-			
-			//minを始点とした各辺を緩和する
+			//min_vを始点とした各辺を緩和する
 			for(Edge e:g.get(min_v)) {
-				dist[e.getTo()] = Math.min(dist[e.getTo()], dist[min_v]+e.getW());
+				dist[e.to]=Math.min(dist[e.to], dist[min_v]+e.w);
 			}
-			used[min_v]=true; //min_vを使用済みとする
+			//探索済み
+			seen[min_v]=true;
 		}
 		//結果出力
-				for(int i=0;i<n;i++) {
-					if(dist[i]==INF) System.out.println(-1);
-					else System.out.println(dist[i]);
-				}
+		for(int i=0;i<n;i++) {
+			if(dist[i]==INF) System.out.println(-1);
+			else System.out.println(dist[i]);
+		}
 	}
 }
